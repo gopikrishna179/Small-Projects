dcab5a393f47749fee4740408a20963f9ef9abed
Maneesha <gopikrishna.atkuri@gmail.com>
2022-10-22 12:02:27 +0530

    added modified watering_system.ino

--- Watering System/watering_system.ino
+++ Watering System/watering_system.ino
@@ -1,7 +1,7 @@
-//ifMoistureIsBiggerThan - this value controls when to water your plant
+
 int ifMoistureIsBiggerThan = 700;
 int numberOfReadingsAboveTreshold = 0;
-//this valu controls how long plants should be watered
+
 int wateringTimeInSeconds = 120;
 void setup() {
 
@@ -11,7 +11,7 @@ void setup() {
 }
 
 void loop() {
-  //to avoid any errors system will water flower only when 10 last readings will be smaller than ifMoistureIsBiggerThan value
+ 
 if(analogRead(A0) > ifMoistureIsBiggerThan){
   numberOfReadingsAboveTreshold += 1;
 }else{
@@ -21,7 +21,7 @@ if(analogRead(A0) > ifMoistureIsBiggerThan){
 if(numberOfReadingsAboveTreshold == 10){
   numberOfReadingsAboveTreshold = 0;
   digitalWrite(9, HIGH);
-  //we have to delay in for loop just in case we would need to delay for a long time
+
   for(int a = 0; a < wateringTimeInSeconds; a++){
     delay(1000);
   }
